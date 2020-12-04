b41df61e69816c6a0833bb609ab0c4b542a49977
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-05 02:54:57 +0300

    Update 2_a.py

--- 2_a.py
+++ 2_a.py
@@ -2,14 +2,14 @@
 """Tarih: 18.11.2020; Saat: 06:02:00"""
 from math import *
 
-"""y = int(input("Yıl: "))
-m = int(input("Ay: "))
-d = int(input("Gün: "))
-h = int(input('Saat: '))"""
 y = 2020
 m = 11
 d = 18
 h = 6
+"""y = int(input("Yıl: "))
+m = int(input("Ay: "))
+d = int(input("Gün: "))
+h = int(input('Saat: '))"""
 # Eğer ay değeri (m) küçük eşit 2 olursa m + 12 ve y - 1 işlemi uygulanır
 if m <= 2:
     m = m + 12
