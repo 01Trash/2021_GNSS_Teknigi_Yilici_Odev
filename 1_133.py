cb36bb3ce99629c9dda9eb8abacb308a0cf37306
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-05 00:27:24 +0300

    Update 1_133.py

--- 1_133.py
+++ 1_133.py
@@ -19,118 +19,117 @@ NN  |    x (m)    |    y (m)    |  Doğrultu (gon)
 34  |  21756.765  |  28874.917  |    80.18273
 32  |  21760.503  |  25496.384  |    276.62136
 """
-"""
-Koordinatları bilinen iki nokta arası mesafe:
-Ara_Mesafe = math.sqrt(((X2 - X1) ** 2) + ((Y2 - Y1) ** 2))
-"""
-#27-34 arası mesafe = k
+"""Koordinatları bilinen iki nokta arası mesafe:
+Ara_Mesafe = math.sqrt(((X2 - X1) ** 2) + ((Y2 - Y1) ** 2))"""
+"""27-34 arası mesafe = k"""
 X_27 = 23312.451
 X_34 = 21756.765
 Y_27 = 27320.592
 Y_34 = 28874.917
 k = math.sqrt(((X_27 - X_34) ** 2) + ((Y_27 - Y_34) ** 2))
-print("k: ", k)
-#27-32 arası mesafe = l,
+#print("k: ", k)
+"""27-32 arası mesafe = l,"""
 X_27 = 23312.451
 X_32 = 21760.503
 Y_27 = 27320.592
 Y_32 = 25496.384
 l = math.sqrt(((X_27 - X_32) ** 2) + ((Y_27 - Y_32) ** 2))
-print("l: ", l)
-#34-32 arası mesafe = g,
+#print("l: ", l)
+"""34-32 arası mesafe = g,"""
 X_34 = 21756.765
 X_32 = 21760.503
 Y_34 = 28874.917
 Y_32 = 25496.384
 g = math.sqrt(((X_34 - X_32) ** 2) + ((Y_34 - Y_32) ** 2))
-print("g: ", g)
+#print("g: ", g)
 
-#32, 27, 24 arası açı = teta / (Ɵ => Teta işareti)
+"""32, 27, 24 arası açı = teta / (Ɵ => Teta işareti)"""
 """Kosinüs teoreminden Ɵ (Teta) açısını hesaplayalım
 g ** 2 = (k ** 2) + (l ** 2) - 2 * k * l * math.cos(teta)
 Teta açısı tek bir tarafa alınırsa =>
 teta = math.acos((k ** 2 + l ** 2 - g **2) / (2 * k * l))"""
 teta = math.acos((k ** 2 + l ** 2 - g **2) / (2 * k * l))
 teta_grad = (200 * teta) / math.pi
-print("teta_grad: ", teta_grad)
-print("teta: ", teta)
+#print("teta_grad: ", teta_grad)
+#print("teta: ", teta)
 
 """Python trigonometrik hesaplarda, radyan ile hesap yaptığı için gradı radyana çevirelim:
 radyan = (grad * math.pi) / 200"""
-#27, 133, 34 arası açı = alfa / (α => Alfa İşareti)
-#alfa = 80.18273 grad
+"""27, 133, 34 arası açı = alfa / (α => Alfa İşareti)"""
+"""alfa = 80.18273 grad"""
 alfa_grad = 80.18273
 alfa = (alfa_grad * math.pi) / 200
-print("alfa: ", alfa)
+#print("alfa: ", alfa)
 
-#32, 133, 27 arası açı = gama / (Ɣ => Gama İşareti)
-#gama = (400 - 276.62136) grad
+"""32, 133, 27 arası açı = gama / (Ɣ => Gama İşareti)
+#gama = (400 - 276.62136) grad"""
 gama_grad = (400 - 276.62136)
 gama = (gama_grad * math.pi) / 200
-print("gama: ", gama)
+#print("gama: ", gama)
 
-#27, 133, 32 arası açı = beta / (β => Beta İşareti)
-#beta = 276.62136 grad
+"""27, 133, 32 arası açı = beta / (β => Beta İşareti)
+#beta = 276.62136 grad"""
 beta_grad = 276.62136 - 80.18273
 beta = (beta_grad * math.pi) / 200
-print("beta: ", beta)
+#print("beta: ", beta)
 
 """Semt açısı hesabı
 #27-34 => math.atan(semt_27_34) = (Y_34 - Y_27) / (X_34 - X_27)"""
 semt_27_34 = math.atan((Y_34 - Y_27) / (X_34 - X_27)) + math.pi
 semt_27_34_grad = (200 * semt_27_34) / math.pi
-print("semt_27_34_grad: ", semt_27_34_grad)
-print("semt_27_34: ", semt_27_34)
+#print("semt_27_34_grad: ", semt_27_34_grad)
+#print("semt_27_34: ", semt_27_34)
 
 """Semt açısı hesabı
 #27-32 => math.atan(semt_27_32) = (Y_32 - Y_27) / (X_32 - X_27)"""
 semt_27_32 = math.atan((Y_32 - Y_27) / (X_32 - X_27)) + math.pi
 semt_27_32_grad = (200 * semt_27_32) / math.pi
-print("semt_27_32_grad: ", semt_27_32_grad)
-print("semt_27_32: ", semt_27_32)
+#print("semt_27_32_grad: ", semt_27_32_grad)
+#print("semt_27_32: ", semt_27_32)
 
-#beta = a4 + a3 + teta => a4 + a3 = beta - teta
+"""#beta = a4 + a3 + teta => a4 + a3 = beta - teta"""
 a4_a3_toplam_grad = beta_grad - teta_grad
 a4_a3_toplam_yarim_grad = a4_a3_toplam_grad / 2
 a4_a3_toplam_yarim = (a4_a3_toplam_yarim_grad * math.pi) / 200
-print("a4 + a3: ", a4_a3_toplam_grad)
-print("(a4 + a3) / 2: ", a4_a3_toplam_yarim_grad)
+#print("a4 + a3: ", a4_a3_toplam_grad)
+#print("(a4 + a3) / 2: ", a4_a3_toplam_yarim_grad)
 
-#u hesabı
+"""#u hesabı"""
 u = math.atan((l * math.sin(alfa)) / (k * math.sin(gama)))
 u_grad = (200 * u) / math.pi
-print("u_grad: ", u_grad)
+#print("u_grad: ", u_grad)
 
-#math.tan((a4 - a3) / 2) = math.tan((a4 + a3) / 2) * math.cot(50 + u)
+"""#math.tan((a4 - a3) / 2) = math.tan((a4 + a3) / 2) * math.cot(50 + u)"""
 a4_a3_fark_yarim = math.atan(math.tan(a4_a3_toplam_yarim) * (1 / math.tan((math.pi / 4) + u)))
 a4_a3_fark_yarim_grad = (200 * a4_a3_fark_yarim) / math.pi
-print("(a4 - a3) / 2: ", a4_a3_fark_yarim)
-print("(a4 - a3) / 2: ", a4_a3_fark_yarim_grad)
+#print("(a4 - a3) / 2: ", a4_a3_fark_yarim)
+#print("(a4 - a3) / 2: ", a4_a3_fark_yarim_grad)
 
 """(a4 + a3) / 2 + (a4 - a3) / 2 = a4_a3_toplam_yarim_grad + a4_a3_fark_yarim_grad =>
 a4 / 2 + a4 / 2 = a4_a3_toplam_yarim_grad + a4_a3_fark_yarim_grad"""
 a4_grad = a4_a3_toplam_yarim_grad + a4_a3_fark_yarim_grad
-print("a4_grad: ", a4_grad)
+#print("a4_grad: ", a4_grad)
 a3_grad = 2 * a4_a3_toplam_yarim_grad - a4_grad
 a3 = (200 * a3_grad) / math.pi
-print("a3_grad: ", a3_grad)
+#print("a3_grad: ", a3_grad)
 a2_grad = 200 - gama_grad - a4_grad
-print("a2_grad: ", a2_grad)
+#print("a2_grad: ", a2_grad)
 a1_grad = 200 - alfa_grad - a3_grad
-print("a1_grad: ", a1_grad)
+#print("a1_grad: ", a1_grad)
 
 """Semt açısı hesabı
 semt_27_133_grad = semt_27_34_grad + a1_grad"""
 semt_27_133_grad = semt_27_34_grad + a1_grad
 semt_27_133 = (semt_27_133_grad / math.pi) * 200
-print("semt_27_133_grad: ", semt_27_133_grad)
+#print("semt_27_133_grad: ", semt_27_133_grad)
 
 """#27-133 arası mesafe = m"""
 m = (k / math.sin(alfa)) * math.sin(a3)
-print("m: ", m)
+#print("m: ", m)
 
 """133 noktasının koordinat hesabı"""
 Y_133 = Y_27 + m * math.sin(semt_27_133)
 X_133 = X_27 + m * math.cos(semt_27_133)
-print("133 x: ", X_133)
-print("133 y: ", Y_133)
+print("133 x: ", round(X_133,3))
+print("133 y: ", round(Y_133,3))
+
