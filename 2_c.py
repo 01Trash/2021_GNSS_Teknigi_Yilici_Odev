9a8f98450266422e76d9daba04615f2b36472370
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-07 14:02:57 +0300

    Update 2_c.py

--- 2_c.py
+++ 2_c.py
@@ -2,23 +2,18 @@
 """Tarih: 18.11.2020; Saat: 06:02:00"""
 from math import *
 
-y = 2020
-m = 11
-d = 18
-h = 6
-"""y = int(input("Yıl: "))
-m = int(input("Ay: "))
-d = int(input("Gün: "))
-h = int(input('Saat: '))"""
-# Eğer ay değeri (m) küçük eşit 2 olursa m + 12 ve y - 1 işlemi uygulanır
-if m <= 2:
-    m = m + 12
-    y = y - 1
-# Jülyen günü hesabı JD
-JD = floor(365.25 * y) + floor(30.6004 * (m + 1)) + d + (h / 24) + 1720981.5
-#print("Jülyen günü: ", JD)
+saat = 6
+dakika = 2
+saniye = 0
+"""saat = int(input("Saat: "))
+dakika = int(input("Dakika: "))
+saniye = int(input("Saniye: "))"""
 
-# Yersel (TT) zaman hesabı TT
-#JD = (TT - 51544.5) / 36525 =>
-TT = JD * 26525 + 51544.5
-print("Yersel (TT) zaman: ", TT)
+"""TT = TAI + 32.184 saniye
+TAI = UTC + 37 saniye"""
+TAI = 0 + 37
+#print("TAI: 06:02:37")
+TT = 37 + 32.183
+print("TT => 06:03:09.184")
+
+
