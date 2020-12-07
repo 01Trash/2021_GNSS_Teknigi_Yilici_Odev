38885ab9b3746d2fe5d1b748eb0f262dde427069
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-07 14:34:55 +0300

    Update 2_b.py

--- 2_b.py
+++ 2_b.py
@@ -10,33 +10,33 @@ dakika = int(input("Dakika: "))
 saniye = int(input("Saniye: "))"""
 
 #GPS = UTC + 18 saniye
-saat = 6 + 0
-dakika = 2 + 0
-saniye = 0 + 18
+saat = saat + 0
+dakika = dakika + 0
+saniye = saniye + 18
 if saniye >= 60:
     saniye = saniye % 60
     dakika = dakika + 1
 print("GPS => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
 
 #GLONASS = UTC + 3 saat
-saat = 6 + 3
-dakika = 2 + 0
-saniye = 0 + 0
+saat = saat + 3
+dakika = dakika + 0
+saniye = saniye + 0
 print("GLONASS => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
 
 #BEIOU = UTC + 4 saniye
-saat = 6 + 0
-dakika = 2 + 0
-saniye = 0 + 4
+saat = saat + 0
+dakika = dakika + 0
+saniye = saniye + 4
 if saniye >= 60:
     saniye = saniye % 60
     dakika = dakika + 1
 print("BEIOU => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
 
 #GALILEO = UTC + 18 saniye
-saat = 6 + 0
-dakika = 2 + 0
-saniye = 0 + 18
+saat = saat + 0
+dakika = dakika + 0
+saniye = saniye + 18
 if saniye >= 60:
     saniye = saniye % 60
     dakika = dakika + 1
