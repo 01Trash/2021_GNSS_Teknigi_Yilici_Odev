3d40768365184f1615039ccf3fe6e50868241bc1
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-10 18:13:04 +0300

    Update 2_e.py

--- 2_e.py
+++ 2_e.py
@@ -23,10 +23,13 @@ t = (JD - 2451545.0) / 36525
 #print("t: ", t)
 
 a = 2306.2181 * t + 0.30188 * t * t + 0.017998 * t * t * t
+a = a / 3600
 #print("a: ", a)
 b = 2004.3109 * t - 0.42665 * t * t - 0.041833 * t * t * t
+b = b / 3600
 #print("b: ", b)
 c = 2306.218 * t + 1.09468 * t * t + 0.018203 * t * t * t
+c = c / 3600
 #print("c: ", c)
 #Grad = (Radyan * pi) / 200
 a = (a * pi) / 200
@@ -44,7 +47,7 @@ Pmatris_3_1 = cos(a) * sin(b)
 Pmatris_3_2 = - sin(a) * sin(b)
 Pmatris_3_3 = cos(b)
 #Ekrana yazdırma
-print("       |  {:f}    {:f}    {:f}  |" .format(Pmatris_1_1, Pmatris_1_2, Pmatris_1_3))
+print("       |   {:f}    {:f}    {:f}  |" .format(Pmatris_1_1, Pmatris_1_2, Pmatris_1_3))
 print("P(t) = |   {:f}     {:f}    {:f}  |" .format(Pmatris_2_1, Pmatris_2_2, Pmatris_2_3))
 print("       |   {:f}    {:f}     {:f}  |" .format(Pmatris_3_1, Pmatris_3_2, Pmatris_3_3))
 print("------------------------------------------------")
@@ -60,6 +63,7 @@ Nmatris_3_2 = cos(c) * sin(a) * cos(b) - sin(c) * cos(a)
 Nmatris_3_3 = sin(c) * sin(a) * cos(b) + cos(c) * cos(a)
 #Ekrana yazdırma
 print("       |   {:f}    {:f}    {:f}  |" .format(Nmatris_1_1, Nmatris_1_2, Nmatris_1_3))
-print("N(t) = |   {:f}     {:f}    {:f}  |" .format(Nmatris_2_1, Nmatris_2_2, Nmatris_2_3))
+print("N(t) = |   {:f}     {:f}     {:f}  |" .format(Nmatris_2_1, Nmatris_2_2, Nmatris_2_3))
 print("       |   {:f}    {:f}     {:f}  |" .format(Nmatris_3_1, Nmatris_3_2, Nmatris_3_3))
+
 
