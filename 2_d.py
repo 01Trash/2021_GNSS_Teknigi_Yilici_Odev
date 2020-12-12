5a2e2775f5f749a1580efbeff9d7a589b2d21d4a
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-12 23:54:25 +0300

    Update 2_d.py

--- 2_d.py
+++ 2_d.py
@@ -2,28 +2,53 @@
 """Tarih: 18.11.2020; Saat: 06:02:00"""
 from math import *
 
-saat = 6
-dakika = 2
-saniye = 0
-"""saat = int(input("Saat: "))
-dakika = int(input("Dakika: "))
-saniye = int(input("Saniye: "))"""
+y = 2020
+m = 11
+d = 18
+h = 6
+minute = 2
+second = 0
+"""y = int(input("Yıl: "))
+m = int(input("Ay: "))
+d = int(input("Gün: "))
+h = int(input('Saat: '))
+minute = int(input('Dakika: '))
+second = int(input('Saniye: '))"""
+# Eğer ay değeri (m) küçük eşit 2 olursa m + 12 ve y - 1 işlemi uygulanır
+if m <= 2:
+    m = m + 12
+    y = y - 1
+# Jülyen günü hesabı
+JD = floor(365.25 * y) + floor(30.6004 * (m + 1)) + d + ((h + minute/60 + second/3600) / 24) + 1720981.5
+#print("Jülyen günü: {:.2f}" .format(JD))
 
-#GMT = 06:02:00 - 3 saat => GMT = 03:02:00
-#GMST0 = 03:49:58.857
-#Delta_R = 29.9 saniye
-"""GMST = GMT - 12 + (GMST0 + 12) + Delta_R"""
-GMST0_saat = 3
-GMST0_dakika = 49
-GMST0_saniye = 58.857
-Delta_R = 29.9
-GMST_saat = saat + GMST0_saat
-GMST_dakika = dakika + GMST0_dakika
-GMST_saniye = saniye + GMST0_saniye + Delta_R
-if GMST_saniye >= 60:
-    GMST_saniye = GMST_saniye % 60
-    GMST_dakika = GMST_dakika + 1
-if GMST_dakika >= 60:
-    GMST_saat = GMST_saat % 60
-print("GMST => {:.0f}:{:.0f}:{:.3f}" .format(GMST_saat, GMST_dakika, GMST_saniye))
+#T0 hesabı
+T0 = (JD - 2451545) / 36525
+#print("T0: ", T0)
+#GMST hesabı
+a1 = 6 * 3600 + 41 * 60 + 50.54841
+#print(a1)
+a2 = 8640184.812866 * T0
+#print(a2)
+a3 = 0.093104 * T0 * T0
+#print(a3)
+a4 = (6.2 * pow(10, -6)) * T0 * T0 * T0
+#print(a4)
+GMST = a1 + a2 + a3 - a4
+#print("GMST => {:.2f}" .format(GMST))
+
+GMST_hour = 0
+while GMST > 3600:
+    GMST = GMST - 3600
+    GMST_hour = GMST_hour + 1
+GMST_minute = 0
+while GMST > 60:
+    GMST = GMST - 60
+    GMST_minute = GMST_minute + 1
+GMST_second = GMST
+
+#print(GMST_hour)
+#print(GMST_minute)
+#print(GMST_second)
+print("GMST => {:.0f} s {:.0f} d {:f} s" .format(GMST_hour, GMST_minute, GMST_second))
 
