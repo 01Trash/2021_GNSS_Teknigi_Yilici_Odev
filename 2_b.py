cd4af9a66bb2b49ac81b3278f26fcff9a9a47895
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-12 22:27:33 +0300

    Update 2_b.py

--- 2_b.py
+++ 2_b.py
@@ -10,35 +10,40 @@ dakika = int(input("Dakika: "))
 saniye = int(input("Saniye: "))"""
 
 #GPS = UTC + 18 saniye
-saat = saat + 0
-dakika = dakika + 0
-saniye = saniye + 18
-if saniye >= 60:
-    saniye = saniye % 60
-    dakika = dakika + 1
-print("GPS => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
+saat_gps = saat + 0
+dakika_gps = dakika + 0
+saniye_gps = saniye + 18
+if saniye_gps >= 60:
+    saniye_gps = saniye_gps % 60
+    dakika_gps = dakika_gps + 1
+print("GPS => {:.0f}:{:.0f}:{:.0f}" .format(saat_gps, dakika_gps, saniye_gps))
 
 #GLONASS = UTC + 3 saat
-saat = saat + 3
-dakika = dakika + 0
-saniye = saniye + 0
-print("GLONASS => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
+saat_glonass = saat + 3
+dakika_glonass = dakika + 0
+saniye_glonass = saniye + 0
+if saniye_glonass >= 60:
+    saniye_glonass = saniye_glonass % 60
+    dakika_glonass = dakika_glonass + 1
+print("GLONASS => {:.0f}:{:.0f}:{:.0f}" .format(saat_glonass, dakika_glonass, saniye_glonass))
 
 #BEIOU = UTC + 4 saniye
-saat = saat + 0
-dakika = dakika + 0
-saniye = saniye + 4
-if saniye >= 60:
-    saniye = saniye % 60
-    dakika = dakika + 1
-print("BEIOU => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
+saat_beiou = saat + 0
+dakika_beiou = dakika + 0
+saniye_beiou = saniye + 4
+if saniye_beiou >= 60:
+    saniye_beiou = saniye_beiou % 60
+    dakika_beiou = dakika_beiou + 1
+print("BEIOU => {:.0f}:{:.0f}:{:.0f}" .format(saat_beiou, dakika_beiou, saniye_beiou))
 
 #GALILEO = UTC + 18 saniye
-saat = saat + 0
-dakika = dakika + 0
-saniye = saniye + 18
-if saniye >= 60:
-    saniye = saniye % 60
-    dakika = dakika + 1
-print("GALILEO => {:.0f}:{:.0f}:{:.0f}" .format(saat, dakika, saniye))
+saat_galileo = saat + 0
+dakika_galileo = dakika + 0
+saniye_galileo = saniye + 18
+if saniye_galileo >= 60:
+    saniye_galileo = saniye_galileo % 60
+    dakika_galileo = dakika_galileo + 1
+print("GALILEO => {:.0f}:{:.0f}:{:.0f}" .format(saat_galileo, dakika_galileo, saniye_galileo))
+
+
 
