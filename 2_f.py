75550a4377ad56ac1fb9f51e9c867791d7170c68
Erdem <57351007+ErdemGns@users.noreply.github.com>
2020-12-13 21:30:09 +0300

    Update 2_f.py

--- 2_f.py
+++ 2_f.py
@@ -15,10 +15,11 @@ Wmatris_3_1 = - x_p
 Wmatris_3_2 = y_p
 Wmatris_3_3 = 1
 #Ekrana yazdırma
-print("       |    {:.5f}     {:.5f}     {:.5f}  |" .format(Wmatris_1_1, Wmatris_1_2, Wmatris_1_3))
-print("W(t) = |    {:.5f}     {:.5f}    {:.5f}  |" .format(Wmatris_2_1, Wmatris_2_2, Wmatris_2_3))
-print("       |   {:.5f}     {:.5f}     {:.5f}  |" .format(Wmatris_3_1, Wmatris_3_2, Wmatris_3_3))
+print("       |   {:.5f}     {:.5f}     {:.5f}  |" .format(Wmatris_1_1, Wmatris_1_2, Wmatris_1_3))
+print("W(t) = |   {:.5f}     {:.5f}    {:.5f}  |" .format(Wmatris_2_1, Wmatris_2_2, Wmatris_2_3))
+print("       |  {:.5f}     {:.5f}     {:.5f}  |" .format(Wmatris_3_1, Wmatris_3_2, Wmatris_3_3))
+print("---------------------------------------------")
 
 #Determinant hesabı
 matrix = np.array([[1,0,0.00009],[0,1,-0.00009],[-0.00009,0.00009,1]])
-print("Determinant: {:.13f}" .format(np.linalg.det(matrix)))
+print("Determinant W(t): {:.13f}" .format(np.linalg.det(matrix)))
